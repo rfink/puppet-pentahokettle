f3ed03f4699495709cb661f5f069076b9b1f16b4
Juan Sierra Pons <juan@elsotanillo.net>
2015-10-14 16:57:27 +0200

    regsubst seems buggy. Overriding

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,9 @@
 class pentahokettle {
   $version = '5.3.0.0-213'
   $mySqlConnector = 'mysql-connector-java-5.1.34-bin.jar'
-  $subVersion = regsubst($version, '/^[0-9]+\.[0-9]+(.*)/', '')
+  #$subVersion = regsubst($version, '/^[0-9]+\.[0-9]+(.*)/', '')
+  ## regsubst seems buggy. Overriding
+  $subVersion = '5.3'
   $url = "http://sourceforge.net/projects/pentaho/files/Data%20Integration/${subVersion}/pdi-ce-${version}.zip/download"
   $tmpDest = '/tmp/pdi-ce.zip'
   $destDir = '/opt/data-integration'
