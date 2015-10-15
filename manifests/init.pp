412fdae30e4ded88daea7a4741ff1c8b8b197975
Juan Sierra Pons <juan@elsotanillo.net>
2015-10-15 09:24:31 +0200

    regexp expression fixed

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,11 +6,8 @@
 class pentahokettle {
   $version = '5.3.0.0-213'
   $mySqlConnector = 'mysql-connector-java-5.1.36-bin.jar'
-  $subVersionTEST = regsubst($version, '^([0-9]+\.[0-9])+(.*)', '\1')
-  notify { $version: }
+  $subVersion = regsubst($version, '^([0-9]+\.[0-9])+(.*)', '\1')
   notify { $subVersionTEST: }
-  ## regsubst seems buggy. Overriding
-  $subVersion = '5.3'
   $url = "http://sourceforge.net/projects/pentaho/files/Data%20Integration/${subVersion}/pdi-ce-${version}.zip/download"
   $tmpDest = '/tmp/pdi-ce.zip'
   $destDir = '/opt'
